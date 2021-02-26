6eaf16feb215495e08d79e5534c9ff2bf6569cd0
hezi3188 <hezi3188@gmail.com>
2021-02-26 10:41:47 +0200

    finish users controller&BL

--- server/CinemaWS/controllers/usersController.js
+++ server/CinemaWS/controllers/usersController.js
@@ -4,45 +4,142 @@ const usersBL=require('../models/usersBL');
 
 const router=express.Router();
 
+const RSA_PRIVATE_KEY="dsadcdf23dffdfr3r3";
+
+const checkAuth=(token)=> {
+    let id=null;
+    jwt.verify(token, RSA_PRIVATE_KEY, function(err, decoded) 
+    { 
+        if(decoded)
+             id=decoded.id;
+    });
+    return id;
+} 
+
+
+
 router.route('/')
 .get(async function(req, res){
-    let data=await usersBL.getAll();
-    return res.json(data);
+    var token = req.headers['x-access-token'];
+    if (!token){
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    }
+    if(checkAuth(token)) {
+        let data;
+        try {
+             data=await usersBL.getAll();
+        } catch (error) {
+            return res.status(500).send("Error: " + error.message);
+        }
+        return res.json(data);
+    }
+    else{
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    }
+  
 })
 
 router.route('/:id')
 .get(async function(req, res){
-    let id=req.params.id;
-    let data=await usersBL.getUser(id);
-    return res.json(data);
+    var token = req.headers['x-access-token'];
+    if(!token) {
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    }
+    if(checkAuth(token)){
+        let id=req.params.id;
+        let data;
+        try {
+            data=await usersBL.getUser(id);
+        } catch (error) {
+            return res.status(500).send("Error: " + error.message);
+        }
+        return res.json(data);
+    }
+    else{
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    }
+   
 })
 
 router.route('/')
 .post(async function(req, res){
+    var token = req.headers['x-access-token'];
+    if(!token) {
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    }
+    if(checkAuth(token)){
         let obj=req.body;
-        let data=await usersBL.addUser(obj);
+        let data;
+        try {
+            data=await usersBL.addUser(obj);
+        } catch (error) {
+            return res.status(500).send("Error: " + error.message);
+        }
         return res.json(data);
+    }
+    else{
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    } 
 })
 
 router.route('/:id')
 .put(async function(req, res){
-    let id=req.params.id;
-    let obj=req.body;
-    let data=await usersBL.updateUser(id,obj);
-    return res.json(data);
+    var token = req.headers['x-access-token'];
+    if(!token) {
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    }
+    if(checkAuth(token)){
+
+
+        let id=req.params.id;
+        let obj=req.body;
+        let data;
+        try {
+            data=await usersBL.updateUser(id,obj);
+        } catch (error) {
+            return res.status(500).send("Error: " + error.message);
+        }
+        return res.json(data);
+    }
+    else{
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    } 
 })
+
 router.route('/:id')
 .delete(async function(req, res){
-    let id=req.params.id;
-    let data=await usersBL.deleteUser(id);
-    return res.json(data);
+    var token = req.headers['x-access-token'];
+    if(!token) {
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    }
+    if(checkAuth(token)){
+
+
+        let id=req.params.id;
+        let data;
+        try {
+             data=await usersBL.deleteUser(id);
+        } catch (error) {
+            return res.status(500).send("Error: " + error.message);
+        }
+            return res.json(data);
+    }
+    else{
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    } 
 })
 router.post('/register',async function(req, res){
     const username=req.body.userName;
     const password=req.body.password;
 
-    let data=await usersBL.register(username,password);
-    return res.json(data);
+    try {
+        let data=await usersBL.register(username,password);
+        const id= await usersBL.getUserId(username);
+        var userToken=jwt.sign({id:id},RSA_PRIVATE_KEY,{expiresIn:await usersBL.getSeesionTimeOut(id)*60});
+        res.status(200).send({msg:data,token:userToken});
+    } catch (error) {
+        res.status(401).send("Error: "+error.message);
+    }
 });
 
 
@@ -50,7 +147,6 @@ router.post('/login',async function(req, res){
     const username=req.body.userName;
     const password=req.body.password;
 
-    const RSA_PRIVATE_KEY="dsadcdf23dffdfr3r3";
 
     try {
         const id= await usersBL.getUserId(username);
@@ -66,7 +162,7 @@ router.post('/login',async function(req, res){
         }
     }
     catch(err){
-        res.status(401).send(err.message);
+        res.status(401).send("Error: "+err.message);
     }
 })
 module.exports=router;
--- server/CinemaWS/models/usersBL.js
+++ server/CinemaWS/models/usersBL.js
@@ -3,20 +3,325 @@ const axios=require('axios');
 const jfile=require('jsonfile');
 
 
+exports.getAll=async function(){
+    //get all data
+    const usersData = await getAllUsersData();
+    const permissions = await getAllUsersPermissions();
+    const usersName = await getAllUserFromDB();
+
+
+
+    //create data for client
+    let users=[];
+    for (let i = 0; i < usersData.length; i++) {
+        users[i]={
+            id: usersData[i].id,
+            firstName:usersData[i].firstName,
+            lastName:usersData[i].lastName,
+            username:usersName[i].userName,
+            sessionTimeOut:usersData[i].sessionTimeOut,
+            created:usersData[i].created,
+            permissions:permissions[i].permissions
+        };
+    }
+    return users;
+  
+}
+
+exports.getUser=async function(id){
+    let userData=await getUserData(id);
+    if(!userData){
+        throw new Error ("no user with that id");
+    }
+    let permissions=await getUserPermission(id);
+
+
+
+    return new Promise(function(resolve, reject) {
+        User.findById(id, function(err, data){
+            if(err) {
+                reject(err);
+            }
+            else{
+                let user={
+                    id: userData.id,
+                    firstName:userData.firstName,
+                    lastName:userData.lastName,
+                    username:data.userName,
+                    sessionTimeOut:userData.sessionTimeOut,
+                    created:userData.created,
+                    permissions:permissions.permissions
+                };
+                resolve(user);
+            }
+        })
+    })
+}
+
+exports.addUser=async function(obj)
+{
+    let newUser;
+    if(await isUserNameExist(obj.userName)){
+        throw new Error("The user name already exists");
+    }
+    else{
+
+        //add new user to mongodb
+        newUser=new User({
+            userName:obj.userName
+        });
+        newUser.save(function(err) {
+            if(err){
+                throw new Error (err);
+            }
+        });
+
+
+
+
+        //add new user data to the users file
+        const newUserData={
+            id:newUser._id,
+            firstName:obj.firstName,
+            lastName:obj.lastName,
+            created:new Date(),
+            sessionTimeOut:obj.sessionTimeOut
+        };
+        const newUserPermissions={
+            id:newUser._id,
+            permissions:obj.permissions
+        };
+
+        let usersData=await getAllUsersData();
+        usersData=[...usersData,newUserData];
+        jfile.writeFile('./dataSource/Users.json',usersData,function(err){
+            if(err){
+                throw new Error (err);
+            }
+        });
+
+
+
+        //add new user permission for the permissions file
+        let permissions=await getAllUsersPermissions();
+        permissions=[...permissions,newUserPermissions];
+        jfile.writeFile('./dataSource/Permissions.json',permissions,function(err){
+            if(err){
+                throw new Error(err);
+            }
+        });
+     }
+
+     
+    return "Created new user with id: "+newUser._id;
+         
+}
+
+exports.updateUser=async function(id,obj){
+
+    let currentUserName=await getUserFromDB(id);
+    currentUserName=currentUserName.userName;
+   
+    //if the user change the username
+    if(currentUserName!=obj.userName){
+        if(await isUserNameExist(obj.userName)){
+            throw new Error("The username is already in use");
+        }
+        else{
+            //update on mongodb
+            User.findByIdAndUpdate(id,
+                {
+                    userName : obj.userName,
+                },function(err, user){
+                    if(err){
+                        throw new Error(err);
+                    }
+            });
+        }
+    }
+    
+ 
+
+    //update users data file
+    let userData=await getUserData(id);
+    const newUserData={
+        id:userData.id,
+        firstName:obj.firstName,
+        lastName:obj.lastName,
+        created:userData.created,
+        sessionTimeOut:obj.sessionTimeOut
+    };
+    let usersData=await getAllUsersData();
+    usersData=usersData.map(user=>{
+        if(user.id==id){
+            return newUserData;
+        }
+        return user;
+    });
+    jfile.writeFile('./dataSource/Users.json',usersData,function(err){
+        if(err){
+            throw new Error(err);
+        }
+    });
+
+
+    //update permissions file
+    const newUserPermissions={
+        id:userData.id,
+        permissions:obj.permissions
+    };
+    let permissions=await getAllUsersPermissions();
+    permissions=permissions.map(user=>{
+        if(user.id==id){
+            return newUserPermissions;
+        }
+        return user;
+    });
+    jfile.writeFile('./dataSource/Permissions.json',permissions,function(err){
+        if(err){
+            error=err;
+        }
+    });
+    return "Updated user id: "+id+" successfully!";
+}
+
+
+exports.deleteUser=async function(id){
+    //delete from mongodb
+    let error;
+    try {
+        await User.findByIdAndRemove(id, function(err, user){
+            if(user==null||err){
+                error="the id not valid or we can't connect to dada base";
+            }
+          
+        });
+    } catch (error) {
+        throw new Error("the id not valid")
+    }
+    
+    if(error)
+    {
+        throw new Error(error);
+    }
+
+
+    //deleted user data from users.json
+    let allUsers=await getAllUsersData();
+    allUsers=allUsers.filter(data=>data.id!=id);
+    jfile.writeFile('./dataSource/Users.json',allUsers,function(err){
+        if(err){
+            throw new Error(err);
+        }
+    });
+
+
+    //deleted user permission from permissions.json
+    let allUsersPermissions=await getAllUsersPermissions();
+    allUsersPermissions=allUsersPermissions.filter((data=>data.id!=id));
+    jfile.writeFile('./dataSource/Permissiosns.json',allUsersPermissions,function(err){
+        if(err){
+            throw new Error(err);
+        }
+    });
+  return "Deleted user id: "+id+ " successfully!";
+}
+
+
+exports.register=async function(userName,password){
+    let id=await this.getUserId(userName);//if no username like this we throw error
+    let user=await getUserFromDB(id);
+  
+
+    if(user.password){
+        throw new Error ("You have already an account, please login!");
+    }
+
+    //update on mongodb
+    User.findByIdAndUpdate(id,{
+        userName:userName,
+        password:password
+    },function(err,user){
+        if(err) throw new Error (err);
+    });
+
+    return "You registered successfully.";
+}
+
+//return true if the username&password are correct
+exports.login=async function(userName,password){
+    let users=await getAllUserFromDB();
+
+    let user=users.filter(data=>data.userName==userName&&data.password==password);
+    if(user.length==0){
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Auxiliary functions
+exports.getUserId=async function(userName){
+    let users= await getAllUserFromDB();
+    let user=users.filter(data=>data.userName==userName);
+    if(user.length==0){
+        throw new Error("The user name is not exist!");
+    }
+    return user[0].id;
+}
+
+exports.getSeesionTimeOut=async function(id){
+    let usersData=await getUserData(id);
+    return usersData.sessionTimeOut;
+}
+
+exports.checkPermission=async (id,permission) =>{
+
+    let userPermission= await getUserPermission(id);
+    let data=userPermission.permissions.filter(per=>per==permission)
+    if(data.length>0){
+        return true;
+    }
+    return false;
+}
+
 const isUserNameExist=async (uname) => {
-    let userNames=await getAllUsersName();
+    let userNames=await getAllUserFromDB();
     
     userNames=userNames.filter(user =>user.userName==uname);
-    console.log(userNames);
     if(userNames.length==0){
         return false;
     }
     return true;
 }
 
-const getAllUsersName=()=>{
+const getAllUserFromDB=()=>{
     return new Promise(function(resolve, reject) {
         User.find({}, function(err, data){
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(data);
+            }
+        })
+    });
+}
+const getUserFromDB=(id)=>{
+    return new Promise(function(resolve, reject) {
+        User.findById(id, function(err, data){
             if(err){
                 reject(err);
             }
@@ -38,6 +343,23 @@ const getAllUsersData=function(){
         });
     });
 }
+
+const getUserPermission=async (id)=>{
+    return new Promise(function(resolve, reject) {
+        jfile.readFile('./dataSource/Permissions.json',function(err,data){
+            if(err){
+                reject(err);
+            }
+            else{
+                let obj= data.filter(data=>data.id==id)
+                if(obj!=[]){
+                resolve(obj[0]);
+                }
+                resolve(null);
+            }
+        });
+   });
+}
 const getAllUsersPermissions=()=>{
     return new Promise(function(resolve, reject) {
         jfile.readFile('./dataSource/Permissions.json',function(err,data){
@@ -55,272 +377,4 @@ const getUserData=async (id) => {
     let users=await getAllUsersData();
     let user=users.filter(data=>data.id==id);
     return user[0];
-}
-
-
-
-exports.getAll=async function(){
-    const usersData = await getAllUsersData();
-    console.log(usersData);
-
-    const permissions = await getAllUsersPermissions();
-    console.log(permissions);
-
-    const usersName = await getAllUsersName();
-    console.log(usersName);
-
-    let users=[];
-
-    for (let i = 0; i < usersData.length; i++) {
-        users[i]={
-            id: usersData[i].id,
-            firstName:usersData[i].firstName,
-            lastName:usersData[i].lastName,
-            username:usersName[i].userName,
-            sessionTimeOut:usersData[i].sessionTimeOut,
-            created:usersData[i].created,
-            permissions:permissions[i].permissions
-        };
-    }
-    return users;
-  
-}
-
-exports.getUser=async function(id){
-    let userData=await getAllUsersData();
-    userData=userData.filter(user=>user.id==id);
-    if(userData.length==0){
-        return "no user with that id";
-    }
-    userData=userData[0];
-    let permissions=await getAllUsersPermissions();
-    permissions=permissions.filter(permission=>permission.id==id);
-    permissions=permissions[0];
-    console.log(permissions);
-    console.log(userData);
-    return new Promise(function(resolve, reject) {
-        User.findById(id, function(err, data){
-            if(err) {
-                reject(err);
-            }
-            else{
-                let user={
-                    id: userData.id,
-                    firstName:userData.firstName,
-                    lastName:userData.lastName,
-                    username:data.userName,
-                    sessionTimeOut:userData.sessionTimeOut,
-                    created:userData.created,
-                    permissions:permissions.permissions
-                };
-                console.log(user)
-                resolve(user);
-            }
-        })
-    })
-}
-
-exports.addUser=async function(obj)
-{
-    if(await isUserNameExist(obj.userName)){
-        return "The user name already exists";
-    }
-    else{
-        const newUser=new User({
-            userName:obj.userName
-        });
-        newUser.save(function(err) {
-            if(err){
-                return err;
-            }
-        });
-
-
-       
-        const newUserData={
-            id:newUser._id,
-            firstName:obj.firstName,
-            lastName:obj.lastName,
-            created:new Date(),
-            sessionTimeOut:obj.sessionTimeOut
-        };
-        const newUserPermissions={
-            id:newUser._id,
-            permissions:obj.permissions
-        };
-
-
-        let usersData=await getAllUsersData();
-        usersData=[...usersData,newUserData];
-        console.log(usersData);
-        jfile.writeFile('./dataSource/Users.json',usersData,function(err){
-            if(err){
-                return err;
-            }
-        });
-
-        let permissions=await getAllUsersPermissions();
-        permissions=[...permissions,newUserPermissions];
-        jfile.writeFile('./dataSource/Permissions.json',permissions,function(err){
-            if(err){
-                return err;
-            }
-        });
-     }
-
-     
-    return "created";
-         
-}
-
-exports.updateUser=async function(id,obj){
-    let error="";
-    User.findByIdAndUpdate(id,
-        {
-            userName : obj.userName,
-        },function(err, user){
-            if(err){
-                error=err;
-            }
-    });
-
-
-    let userData=await getUserData(id);
-    const newUserData={
-        id:userData.id,
-        firstName:obj.firstName,
-        lastName:obj.lastName,
-        created:userData.created,
-        sessionTimeOut:obj.sessionTimeOut
-    };
-    const newUserPermissions={
-        id:userData.id,
-        permissions:obj.permissions
-    };
-
-
-    //update users data file
-    let usersData=await getAllUsersData();
-    usersData=usersData.map(user=>{
-        if(user.id==id){
-            return newUserData;
-        }
-        return user;
-    });
-    jfile.writeFile('./dataSource/Users.json',usersData,function(err){
-        if(err){
-            error=err;
-        }
-    });
-
-
-    //update permissions file
-    let permissions=await getAllUsersPermissions();
-    permissions=permissions.map(user=>{
-        if(user.id==id){
-            return newUserPermissions;
-        }
-        return user;
-    });
-
-    jfile.writeFile('./dataSource/Permissions.json',permissions,function(err){
-        if(err){
-            error=err;
-        }
-    });
-  
-
-    //return
-    if(error){
-        return error;
-    }
-    else{
-        return "updated";
-    }
-}
-
-
-exports.deleteUser=async function(id){
-    User.findByIdAndRemove(id, function(err, user){
-        if(err) {
-            return err;
-        }
-    });
-
-
-
-
-
-    let allUsers=await getAllUsersData();
-    allUsers=allUsers.filter(data=>data.id!=id);
-  
-    
-    jfile.writeFile('./dataSource/Users.json',allUsers,function(err){
-        if(err){
-            return err;
-        }
-    });
-
-
-    let allUsersPermissions=await getAllUsersPermissions();
-    allUsersPermissions=allUsersPermissions.filter((data=>data.id!=id));
-
-    jfile.writeFile('./dataSource/Permissions.json',allUsersPermissions,function(err){
-        if(err){
-            return err;
-        }
-    });
-  return "deleted";
-}
-
-
-exports.register=async function(userName,password){
-    let users=await getAllUsersName();
-    console.log("username: " + userName);
-    let user=users.filter(data=>data.userName==userName);
-
-        console.log(user);
-
-    if(user.length==0){
-        return "The username is not correct!";
-    }
-
-    user=user[0];
-    if(user.password){
-        return "You have already an account, please login!";
-    }
-    let id=user.id;
-    User.findByIdAndUpdate(id,{
-        userName:userName,
-        password:password
-    },function(err,user){
-        if(err) return err;
-    });
-    return "You registered successfully.";
-}
-
-
-exports.login=async function(userName,password){
-    let users=await getAllUsersName();
-
-    let user=users.filter(data=>data.userName==userName&&data.password==password);
-    if(user.length==0){
-        return false;
-    }
-    return true;
-}
-
-
-exports.getUserId=async function(userName){
-    let users= await getAllUsersName();
-    let user=users.filter(data=>data.userName==userName);
-    if(user.length==0){
-        throw new Error("The user name is not exist!");
-    }
-    return user[0].id;
-}
-
-exports.getSeesionTimeOut=async function(id){
-    let usersData=await getUserData(id);
-    return usersData.sessionTimeOut;
 }
